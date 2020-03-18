657701b62e71754351f98284ee0c5c71bc5e99a9
Strom <sankalp66.kumar@gmail.com>
2020-03-18 22:42:58 +0530

    Fake anim controller

---
+++ Assets/_Scripts/AnimationController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+    [SerializeField] private Rigidbody targetRigidBody;
+    [SerializeField] private Animator targetAnimController;
+    
+    // Start is called before the first frame update
+    void Start() {
+           
+    }
+
+    // Update is called once per frame
+    void Update() {
+        targetAnimController.SetFloat("speed", targetRigidBody.velocity.sqrMagnitude > 0 ? 1 : 0f);
+    }
+}
--- Assets/_Scripts/LookAt.cs
+++ Assets/_Scripts/LookAt.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class LookAt : MonoBehaviour
-{
+public class LookAt : MonoBehaviour {
+    public bool onlyIfInRange;
+    public float lookupRange;
     public Transform target;
 
-    void Update()
-    {
-        // Rotate the camera every frame so it keeps looking at the target
-        transform.LookAt(target);
 
+
+    void Update() {
+        // Rotate the camera every frame so it keeps looking at the target
         // Same as above, but setting the worldUp parameter to Vector3.left in this example turns the camera on its side
         //transform.LookAt(target, Vector3.up);
+        if(onlyIfInRange) {
+            var dist = Vector3.Distance(transform.position, target.position);
+            if(dist > lookupRange) {
+                return;
+            }
+        }
+        transform.LookAt(target);
+
     }
 }
